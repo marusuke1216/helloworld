646a2123828b14e5592489a1f8fdb2b63ad3b1c3
marusuke1216 <111682006+marusuke1216@users.noreply.github.com>
2022-08-22 14:36:46 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,32 @@
 import streamlit as st
-import numpy as np
-import altair as alt
-import pandas as pd
-import streamlit as st
+from datetime import time, datetime
 
-st.header('st.write')
+st.header('st.slider')
 
-# Example 1
+st.subheader('slider')
 
-st.write('Hello, *World!* :sunglasses:')
+age = st.slider('How old are you',0,130,25)
+st.write("I'm",age,'years old')
 
-# Example 2
+st.sbheader('Range slider')
 
-st.write(1234)
+values = st.slider(
+  'Salect a range of values',
+  0,0,100.0,(25.0, 75.0))
+  st.write('values:',values)
 
-# Example 3
+st.subheader('Range time slider')
 
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
+appointment = st.slider(
+  "Schedule your appointment:"
+  value=(time(11,30),time(12,45)))
+  st.write("you're scheduled for:",apppintoment)
 
-# Example 4
+st.subheader('datetime slider')
 
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+start_time = st.slider(
+ "When do you start?"
+ value=datetime(2020,1,1,9,30),
+ format="MM/DD/YY - hh:mm")
+ st.write("start timee:",start_time)
+ 
