c88729bcade4f4bcf3e551691a3173b20be86e10
marusuke1216 <orangeorange1216@gmail.com>
2022-08-22 22:16:59 +0900

    day9-4

--- day9.py
+++ day9.py
@@ -1,11 +1,11 @@
-import streamlit as st
+import streamlit as zd
 import pandas as pd
 import numpy as np
 
-st.header('Line chart')
+zd.header('Line chart')
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+zd.line_chart(chart_data)
