7e45ad234adf6cdb22778704f057cab66d98f014
marusuke1216 <orangeorange1216@gmail.com>
2022-08-29 21:39:48 +0900

    kai5

--- streamlit_app_kai.py
+++ streamlit_app_kai.py
@@ -14,22 +14,11 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 
 st.header('Output')
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-  if user_name != '':
-    st.write(f'👋 Hello {user_name}!')
-  else:
-    st.write('👈  Please enter your **name**!')
-
-with col2:
-  if user_emoji != '':
-    st.write(f'{user_emoji} is your favorite **emoji**!')
-  else:
-    st.write('👈 Please choose an **emoji**!')
-
-with col3:
-  if user_food != '':
-    st.write(f'🍴 **{user_food}** is your favorite **food**!')
-  else:
-    st.write('👈 Please choose your favorite **food**!')
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
