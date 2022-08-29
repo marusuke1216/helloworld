04895b991a7ae770d52618d9a835c138f89b46c9
marusuke1216 <orangeorange1216@gmail.com>
2022-08-29 22:37:53 +0900

    kai7

--- streamlit_app_kai.py
+++ streamlit_app_kai.py
@@ -12,7 +12,7 @@ with st.expander('About this app'):
 st.sidebar.header('Input')
 st.sidebar.subheader('Input CSV')
 uploaded_file = st.sidebar.file_uploader("Choose a file")
-st.balloons()
+st.sidebar.balloons()
 
 st.header('Output')
 
