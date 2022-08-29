49f475e447a57cdd7f8e2e4dc056d83c1d178b63
marusuke1216 <orangeorange1216@gmail.com>
2022-08-29 11:10:25 +0900

    day16

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,17 @@
 import streamlit as st
-from datetime import time, datetime
 
-st.header('st.slider')
+st.title('Customizing the theme of Streamlit apps')
 
-st.subheader('slider')
+st.Write('Contents of the '.Steamlit/Config.toml' file of this app')
 
-age = st.slider('How old are you',0,130,25)
-st.write("I'm",age,'years old')
+st.code("""
+ [theme]
+ primaryColor="#F39C12"
+ backgroundColor="#2E86C1"
+ secondaryBackgroundColor="#AED6F1"
+ textColor="#FFFFFF"
+ font="monospace"
+ """)
 
-st.subheader('Range slider')
-
-values = st.slider(
-   'Salect a range of values',
-   0.0,100.0,(25.0,75.0))
-st.write('values:',values)
-
-st.subheader('Range time slider')
-
-appointment = st.slider(
-  "Schedule your appointment:",
-  value=(time(11, 30), time(12, 45)))
-st.write("you're scheduled for:",appointment)
-
-st.subheader('datetime slider')
-
-start_time = st.slider(
-  "When do you start?",
-  value=datetime(2020,1,1,9,30),
-  format="MM/DD/YY - hh:mm")
-st.write("start timee:",start_time)
-
-start_time = st.slider(
-  "When do you start?",
-  value=datetime(2020,1,1,9,30),
-  format="MM/DD/YY - hh:mm")
-st.write("git追加:",start_time)
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
