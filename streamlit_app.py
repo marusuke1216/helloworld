517aff592c9766a4e54c833814e976b965faa844
marusuke1216 <orangeorange1216@gmail.com>
2022-08-29 11:24:56 +0900

    day16-3

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write('Contents of the `.streamlit/config.toml` file of this app')
 
 st.code("""
  [theme]
- primaryColor="#F39C12"
+ primaryColor="zzzzzzz"
  backgroundColor="#2E86C1"
  secondaryBackgroundColor="#AED6F1"
  textColor="#FFFFFF"
