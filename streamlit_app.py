535fbcd0ba0c536065f28d6992732d1e69eea3f8
marusuke1216 <orangeorange1216@gmail.com>
2022-08-31 22:01:10 +0900

    day22

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ with st.form('my_form'):
   milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
   owncup_val = st.checkbox('Bring own cup')
 
-  submitted = st,form_sbumit_button('実行')
+  submitted = st.form_sbumit_button('実行')
 
 if submitted:
     st.markdown(f'''
