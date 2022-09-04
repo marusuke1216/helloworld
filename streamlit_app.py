46497481e68590f83d89013a83f5070d5e5e5613
marusuke1216 <orangeorange1216@gmail.com>
2022-09-04 18:30:35 +0900

    day25

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('st.session_state')
 def lbs_to_kg():
     st.session_state.kg = st.session_state.lbs/2.2046
 def kg_to_lbs():
-    st.session_state.lbs = t.session_state.kg*2.2046
+    st.session_state.lbs = st.session_state.kg*2.2046
 
 st.header('Input')
 col1, spacer, col2 = st.columns([2,1,2])
